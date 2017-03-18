c94bc9c2e028e11f72f5fab4dd8c2d67004b3ddc
vektorel <vektorel@lab2-ogr>
2017-03-18 12:25:41 +0300

    Aspect Orianted Prg

---
+++ OtoparkOtomasyon/src/main/java/com/vektorel/oot/util/AspectAuditer.java
@@ -0,0 +1,46 @@
+package com.vektorel.oot.util;
+
+import java.util.Date;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Service;
+
+import com.vektorel.oot.entity.EBase;
+
+@Service
+@Aspect
+public class AspectAuditer {
+
+	@Before("execution(* com.vektorel.oot.util.BaseDao.save(..))")
+	public void beforeSaving(JoinPoint joinPoint) {
+		Object[] args = joinPoint.getArgs();
+		if(args[0] instanceof EBase){
+			EBase b = (EBase) args[0];
+			b.setEkleyen("TT");
+			b.setEklemeTarihi(new Date());
+		}
+	}
+	
+	@After("execution(* com.vektorel.oot.*.*.*(..))")
+	public void afterSaving(JoinPoint joinPoint) {
+		System.out.println("afterSaving Çaðrýldý");
+	}
+	
+	@Before("execution(* com.vektorel.oot.util.BaseDao.update(..))")
+	public void beforeUpdating(JoinPoint joinPoint) {
+		Object[] args = joinPoint.getArgs();
+		if(args[0] instanceof EBase){
+			EBase b = (EBase) args[0];
+			b.setGuncellemeTarihi(new Date());
+			b.setGuncelleyen("TT");
+		}
+	}
+	
+	
+	public void afterUpdating() {
+
+	}
+}
